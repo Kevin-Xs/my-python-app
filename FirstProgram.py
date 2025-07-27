5e3d478de6fd9507c36842b30ca5fd6c88cf6c17
Kevin Xiong <xiongshiyong@yahoo.com>
2025-07-27 17:58:43 -0400

    Add logging

---
+++ FirstProgram.py
@@ -0,0 +1,24 @@
+# data types
+a = 5
+b = 3
+# c = a  # save a's value in c
+# a = b  # 3=> a
+# b = c
+
+# a = a + b
+# b = a - b
+# a = a - b
+
+product = a * b
+quotient = int(a / b)
+m = a % b   # modular
+m = 
+diff = a - b
+total = a+ b
+
+f = 0.34
+print(diff))
+
+# print( a, b, d, 'R', e,sep="*")
+
+
